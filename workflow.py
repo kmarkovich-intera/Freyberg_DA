832517f61b0d7de966f07032be3d06a79cfd7893
unknown <jtwhite1000@gmail.com>
2021-08-16 19:07:13 -0600

    changed names again!  sorry!

--- workflow.py
+++ workflow.py
@@ -1478,10 +1478,10 @@ if __name__ == "__main__":
     setup_interface("monthly_model_files")
     monthly_ies_to_da("monthly_model_files_template")
     process_complex_target_output('complex_master','monthly_model_files_template','seq_monthly_model_files_template',1 )
-    #run_batch_seq_prior_monte_carlo()
-    # setup_interface("daily_model_files")
-    # run_complex_prior_mc('daily_model_files_template')
-#     plot_prior_mc()
+    run_batch_seq_prior_monte_carlo()
+    setup_interface("daily_model_files")
+    run_complex_prior_mc('daily_model_files_template')
+    plot_prior_mc()
 
     exit()
 
